2cf50187d7cbb2906ffb88add4879ea3e1142138
luis macias <macias.luis@uabc.edu.mx>
2021-06-06 01:04:36 -0700

    REST Framework api_view() Decorator

--- MyProject/api_basic/serializers.py
+++ MyProject/api_basic/serializers.py
@@ -4,6 +4,6 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Article
-		fields= ['id','title','author']
+	#	fields= ['id','title','author','email']
 		
-	
+		fields= '__all__'
